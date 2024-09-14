dad67e583649e51a4a4a1049619b39b6d4219095
Naveen Deva <naveendeva909@gmail.com>
2024-09-14 13:31:02 +0530

    added banner code

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 
 import './App.css'
+import Banner from './components/Banner/Banner'
+import Navbar from './components/Navbar/Navbar'
 
 function App() {
 
   return (
     <>
-      <div>
-        <h1>Hello</h1>
-      </div>
+    <Banner />
+     <Navbar />
     </>
   )
 }
